821c0c824a91ffcdb48f55d8e5dc92f63ae39ac3
Prithivee7 <getprithivee@msitprogram.net>
2023-04-11 22:20:36 +0530

    Commited erroneous test case

--- ci.py
+++ ci.py
@@ -3,4 +3,4 @@ print(f"The version of wandb is {wandb.__version__}")
 print("This is to check if actions are getting triggered")
 
 print("Checking the version for testing")
-assert wandb.__version__ == "2.1.01", f"Expected version 2.1.01 but got {wandb.__version__}"
+assert wandb.__version__ == "0.14.2", f"Expected version 0.14.2 but got {wandb.__version__}"
